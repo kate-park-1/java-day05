6440980464638aecd53fb63531c626388f90e8cd
kate-park-1 <60906310+kate-park-1@users.noreply.github.com>
2023-04-10 11:47:33 +0900

    java 5일차

---
+++ src/ex5/exercise/PersonTest.java
@@ -0,0 +1,14 @@
+package ex5.exercise;
+
+public class PersonTest {
+  public static void main(String[] args) {
+    Person[] people = new Person[3];
+    people[0] = new Person("길동이", 22);
+    people[1] = new Student("황진이", 23, 100);
+    people[2] = new ForiegnStudent("Amy", 30, 200, "U.S.A");
+
+    for (Person person : people) {
+      person.show();
+    }
+  }
+}
